884c2cd3388d7f1447e76ca9297a24445b6a1806
Walter Benavides <wbenafi@gmail.com>
2024-07-27 13:18:10 -0600

    Solving social media buttons not hiding completly

--- src/components/homepage/SocialMediaButtons.tsx
+++ src/components/homepage/SocialMediaButtons.tsx
@@ -16,25 +16,28 @@ const SOCIAL_MEDIA_BUTTONS = [
   },
   {
     name: "TikTok",
-    icon: <i className='bx bxl-tiktok text-[36px] md:text-[50px] -mt-[1px] md:-mt-[2px] -ml-[3px] text-white'></i>,
+    icon: (
+      <i className="bx bxl-tiktok text-[36px] md:text-[50px] -mt-[1px] md:-mt-[2px] -ml-[3px] text-white"></i>
+    ),
     link: "https://www.tiktok.com/@tusanvicr",
-  }
+  },
 ];
 
 export default function SocialMediaButtons() {
-
   const scrollY = useWindowScroll();
 
   return (
     <div className="absolute top-0 w-screen z-1">
       <div className="relative w-full h-svh overflow-hidden">
-        <div className="absolute z-10 flex items-center justify-end transition-all duration-1000 ease-in-out bottom-8 gap-x-4 md:gap-x-6 mr-5 md:mr-10" style={{ right: scrollY > 20 ? -160 : 0 }}>
+        <div
+          className="absolute z-10 flex items-center justify-end transition-all duration-1000 ease-in-out bottom-8 gap-x-4 md:gap-x-6 mr-5 md:mr-10"
+          style={{ right: scrollY > 20 ? -250 : 0 }}
+        >
           {SOCIAL_MEDIA_BUTTONS.map(({ name, icon, link }) => {
-            return <SocialMediaButton icon={icon} key={name} link={link} />
+            return <SocialMediaButton icon={icon} key={name} link={link} />;
           })}
         </div>
       </div>
     </div>
-  )
+  );
 }
-
