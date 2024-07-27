072f798a61d62a152f681253769fae2f062b5b38
Walter Benavides <wbenafi@gmail.com>
2024-07-27 12:37:18 -0600

    Adding custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [animations],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "my-pink": "#e6007e",
+        "my-purple": "#604696",
+        "my-blue": "#0099d7",
+        "my-green": "#76b82a",
+        "my-yellow": "#ffed00",
+      },
+    },
+  },
+  plugins: [animations],
+};
